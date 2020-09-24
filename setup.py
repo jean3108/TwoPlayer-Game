22c3654de7e9fcdcfb63768355dc533b1eb59359
jean3108 <jean.soler3108@gmail.com>
2020-09-24 12:25:06 +0200

    Delete requirement

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setuptools.setup(
    long_description_content_type="text/markdown",
    url="https://github.com/jean3108/TwoPlayer-Game",
    packages=setuptools.find_packages(),
-   install_requires=['abc'], 
    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
